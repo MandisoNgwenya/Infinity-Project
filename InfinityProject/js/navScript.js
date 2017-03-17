cdac86c80b6186b43c81f8ddcf0d435db41ebe14
MandisoNgwenya <mandiso04@yahoo.com>
2017-03-17 20:33:37 +0200

    index
    view updates

--- InfinityProject/js/navScript.js
+++ InfinityProject/js/navScript.js
@@ -1,1 +1,15 @@
 ﻿
+function openNav() {
+    document.getElementById("mySidenav").style.width = "200px";
+    document.getElementById("mySidenav").style.height = "20000px";
+
+
+
+
+}
+
+function closeNav() {
+
+    document.getElementById("mySidenav").style.width = "0";
+
+}
