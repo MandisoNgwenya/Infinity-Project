f47e4a8e7c1dd5cf5d7dc364a0c2ab7336d1cd9d
MandisoNgwenya <mandiso04@yahoo.com>
2017-03-20 18:24:41 +0200

    RolesUpdate
    Roles

---
+++ InfinityProject/Controllers/RolesController.cs
@@ -0,0 +1,42 @@
+﻿using InfinityProject.Models;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace InfinityProject.Controllers
+{
+    public class RolesController : Controller
+    {
+        ApplicationDbContext db = new ApplicationDbContext();
+        public RolesController()
+        {
+            db = new ApplicationDbContext();
+        }
+    
+        public ActionResult Index()
+        {
+            var Roles = db.Roles.ToList();
+
+            return View(Roles);
+        }
+        public ActionResult Create()
+        {
+            var Role = new IdentityRole();
+            return View(Role);
+
+        }
+        [HttpPost]
+        public ActionResult Create(IdentityRole Role)
+        {
+            db.Roles.Add(Role);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+
+        }
+
+
+    }
+}
