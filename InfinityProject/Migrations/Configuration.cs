e40cf6f94ff9d22a54bdc543504f5fac5c35180f
Mandiso Ngwenya <21422270@Dut4life.ac.za>
2017-03-22 09:29:50 +0200

    Revert "Revert "updare""
    This reverts commit 7357399a27902dad03c1c82f1ac998c0989f91ac.

--- InfinityProject/Migrations/Configuration.cs
+++ InfinityProject/Migrations/Configuration.cs
@@ -9,7 +9,7 @@ namespace InfinityProject.Migrations
     {
         public Configuration()
         {
-            AutomaticMigrationsEnabled = false;
+            AutomaticMigrationsEnabled = true;
         }
 
         protected override void Seed(InfinityProject.Models.ApplicationDbContext context)
